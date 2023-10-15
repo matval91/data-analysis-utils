b7911b18d6c1002cc392b1ef0f89d6846470d8c4
Matteo Vallar <matteo.vallar@gmail.com>
2023-10-15 22:16:22 +0200

    changed blank spaces

--- train_on_dataset.py
+++ train_on_dataset.py
@@ -2,3 +2,179 @@
 #
 # M. Vallar - 09/2023
 
+#X = train.drop(columns = ['id', 'defects'], axis = 1)
+# Y = train['defects'].map({False: 0, True: 1})
+
+# test_cv = test.drop(columns = ['id'], axis = 1)
+
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.ensemble import RandomForestClassifier, HistGradientBoostingClassifier, GradientBoostingClassifier, ExtraTreesClassifier
+from sklearn.metrics import roc_auc_score, roc_curve, RocCurveDisplay, cohen_kappa_score, log_loss, f1_score
+
+https://www.kaggle.com/code/oscarm524/ps-s3-ep23-eda-modeling-submission
+
+
+ens_cv_scores, ens_preds = list(), list()
+
+sk = RepeatedStratifiedKFold(n_splits = 10, n_repeats = 1, random_state = 42)
+for i, (train_idx, test_idx) in enumerate(sk.split(X, Y)):
+
+    X_train, X_test = X.iloc[train_idx], X.iloc[test_idx]
+    Y_train, Y_test = Y.iloc[train_idx], Y.iloc[test_idx]
+    
+    print('----------------------------------------------------------')
+    
+    train_random_forest(X_train, Y_train, X_test, Y_test, test_cv)
+    
+    #################
+    ## Extra Trees ##
+    #################
+
+
+    ET_md = ExtraTreesClassifier(n_estimators = 500, 
+                                 max_depth = 7,
+                                 min_samples_split = 15,
+                                 min_samples_leaf = 10).fit(X_train, Y_train)
+
+    ET_pred = ET_md.predict_proba(X_test)[:, 1]
+    ET_score = roc_auc_score(Y_test, ET_pred)
+
+    print('Fold', i, '==> ET oof ROC-AUC score is ==>', ET_score)
+
+    ET_pred_test = ET_md.predict_proba(test_cv)[:, 1]
+
+    ##########################
+    ## HistGradientBoosting ##
+    ##########################
+
+    hist_md = HistGradientBoostingClassifier(l2_regularization = 0.01,
+                                             early_stopping = False,
+                                             learning_rate = 0.01,
+                                             max_iter = 500,
+                                             max_depth = 5,
+                                             max_bins = 255,
+                                             min_samples_leaf = 15,
+                                             max_leaf_nodes = 10).fit(X_train, Y_train)
+     
+    hist_pred = hist_md.predict_proba(X_test)[:, 1]
+    hist_score = roc_auc_score(Y_test, hist_pred)
+
+    print('Fold', i, '==> Hist oof ROC-AUC score is ==>', hist_score)  
+
+    hist_pred_test = hist_md.predict_proba(test_cv)[:, 1]
+
+    ##########
+    ## LGBM ##
+    ##########
+
+    LGBM_md = LGBMClassifier(objective = 'binary',
+                             n_estimators = 500,
+                             max_depth = 7,
+                             learning_rate = 0.01,
+                             num_leaves = 20,
+                             reg_alpha = 3,
+                             reg_lambda = 3,
+                             subsample = 0.7,
+                             colsample_bytree = 0.7).fit(X_train, Y_train)
+
+    lgb_pred = LGBM_md.predict_proba(X_test)[:, 1]
+    lgb_score = roc_auc_score(Y_test, lgb_pred)
+
+    print('Fold', i, '==> LGBM oof ROC-AUC score is ==>', lgb_score) 
+
+    lgb_pred_test = LGBM_md.predict_proba(test_cv)[:, 1]
+
+    #########
+    ## XGB ##
+    #########
+
+    XGB_md = XGBClassifier(objective = 'binary:logistic',
+                           tree_method = 'hist',
+                           colsample_bytree = 0.7, 
+                           gamma = 2, 
+                           learning_rate = 0.01, 
+                           max_depth = 7, 
+                           min_child_weight = 10, 
+                           n_estimators = 500, 
+                           subsample = 0.7).fit(X_train, Y_train)
+
+    xgb_pred = XGB_md.predict_proba(X_test)[:, 1]
+    xgb_score = roc_auc_score(Y_test, xgb_pred)
+
+    print('Fold', i, '==> XGB oof ROC-AUC score is ==>', xgb_score)
+
+    xgb_pred_test = XGB_md.predict_proba(test_cv)[:, 1]
+
+    ##############
+    ## CatBoost ##
+    ##############
+
+    Cat_md = CatBoostClassifier(loss_function = 'Logloss',
+                                iterations = 500,
+                                learning_rate = 0.01,
+                                depth = 7,
+                                random_strength = 0.5,
+                                bagging_temperature = 0.7,
+                                border_count = 30,
+                                l2_leaf_reg = 5,
+                                verbose = False, 
+                                task_type = 'CPU').fit(X_train, Y_train)
+
+    cat_pred = Cat_md.predict_proba(X_test)[:, 1]
+    cat_score = roc_auc_score(Y_test, cat_pred)
+
+    print('Fold', i, '==> CatBoost oof ROC-AUC score is ==>', cat_score)
+
+    cat_pred_test = Cat_md.predict_proba(test_cv)[:, 1]    
+    
+    ##############
+    ## Ensemble ##
+    ##############
+    
+    ens_pred_1 = (RF_pred + ET_pred + hist_pred + lgb_pred + xgb_pred + cat_pred) / 6
+    ens_pred_2 = (RF_pred_test + ET_pred_test + hist_pred_test + lgb_pred_test + xgb_pred_test + cat_pred_test) / 6
+    
+    ens_score_fold = roc_auc_score(Y_test, ens_pred_1)
+    ens_cv_scores.append(ens_score_fold)
+    ens_preds.append(ens_pred_2)
+    
+    print('Fold', i, '==> Average Ensemble oof ROC-AUC score is ==>', ens_score_fold)
+
+
+
+
+
+def train_random_forest(X_train, Y_train, X_test, Y_test, test_cv):
+    ########
+    ## RF ##
+    ########
+
+    RF_md = RandomForestClassifier(n_estimators = 500, 
+                                   max_depth = 7,
+                                   min_samples_split = 15,
+                                   min_samples_leaf = 10).fit(X_train, Y_train)
+    
+    RF_pred = RF_md.predict_proba(X_test)[:, 1]
+    RF_score = roc_auc_score(Y_test, RF_pred)
+
+    print('Fold', i, '==> RF oof ROC-AUC score is ==>', RF_score)
+
+    RF_pred_test = RF_md.predict_proba(test_cv)[:, 1]
+    
+def train_extra_trees(X_train, Y_train, X_test, Y_test, test_cv):
+    #################
+    ## Extra Trees ##
+    #################
+
+
+    ET_md = ExtraTreesClassifier(n_estimators = 500, 
+                                 max_depth = 7,
+                                 min_samples_split = 15,
+                                 min_samples_leaf = 10).fit(X_train, Y_train)
+
+    ET_pred = ET_md.predict_proba(X_test)[:, 1]
+    ET_score = roc_auc_score(Y_test, ET_pred)
+
+    print('Fold', i, '==> ET oof ROC-AUC score is ==>', ET_score)
+
+    ET_pred_test = ET_md.predict_proba(test_cv)[:, 1]
